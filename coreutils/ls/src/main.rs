fec8cff0dd28504dad1f4f689aa042c00f1995de
ViridianAStar <noah.yocum@icloud.com>
2024-11-14 00:17:05 -0500

    Begin work on ls. Prints minimal.

--- coreutils/ls/src/main.rs
+++ coreutils/ls/src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::fs;
+use std::io;
+
+// Minimal
+fn list_directory_minimal(path: &str) -> io::Result<()> {
+    let contents = fs::read_dir(path)?;
+
+    for content in contents {
+        let content = content?;
+        println!("{}", content.path().display());
+    }
+
+    Ok(())
+}
+
+/**
+ * Lists directory contents
+ * 
+ * takes arguments
+ */
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    let mut path = "./";
+
+    if args.len() > 1 {
+        path = args[1].as_str();
+    }
+    
+    if let Err(e) = list_directory_minimal(path) {
+        eprintln!("Error reading directory: {}", e);
+    }
 }
