7d28770ba3159eb717ecab9197f3fd4cd018b516
ViridianAStar <noah.yocum@icloud.com>
2024-11-14 21:03:02 -0500

    Implemented FileType enum and some helper functions. TODOs denoting what is yet undone.

--- coreutils/ls/src/main.rs
+++ coreutils/ls/src/main.rs
@@ -1,6 +1,10 @@
+extern crate libc;
 use std::env;
 use std::fs;
 use std::io;
+use std::os::linux::fs::MetadataExt;
+use std::path::Path;
+use libc::{S_IFSOCK, S_IFLNK, S_IFREG, S_IFDIR, S_IFCHR, S_IFBLK, S_IFIFO};
 
 /**
  * Colors for color coding and what not
@@ -118,6 +122,7 @@ impl PrettyPrint {
 /**
  * Enum for file types
  */
+#[derive(Debug)]
 enum FileType {
     Directory,
     SymbolicLink,
@@ -132,7 +137,135 @@ enum FileType {
 }
 
 impl FileType {
+
+    /**
+     * Match metadata to file type
+     */
+    pub fn match_metadata(path_to_file: &Path) -> FileType {
+        match fs::metadata(path_to_file) {
+            Ok(metadata) => {
+                    let mode = metadata.st_mode();
+        
+                    // #TODO: Implement whitespace and arg directory detections
+                    if Self::is_socket(mode) {
+                        return Self::Sock // Socket
+                    } else if Self::is_symlink(mode) {
+                        return Self::SymbolicLink // Symbolic Link
+                    } else if Self::is_regular_file(mode) {
+                        return Self::Normal // Regular / Normal
+                    } else if Self::is_directory(mode) {
+                        return Self::Directory // Directory
+                    } else if Self::is_character_device(mode) {
+                        return Self::Chardev // Character device
+                    } else if Self::is_block_device(mode) {
+                        return Self::Blockdev // Block device
+                    } else if Self::is_fifo(mode) {
+                        return Self::Fifo // Fifo (named pipe)
+                    } else {
+                        return Self::Unknown // Unknown (For everything else)
+                    }
+                }
+            Err(e) => {
+                println!("ERR: cannot read metadata of path {:?} suffering error: {}", path_to_file, e);
+            }
+        }
+
+        return FileType::Unknown
+    }
+
+    /**
+     * Return a string corrosponding to the type
+     */
+    pub fn get_type_short_name(&self) -> &'static str {
+        let mut return_string = "";
+        match *self {
+            Self::Directory => {
+                return_string = "DIR"
+            },
+            Self::SymbolicLink => {
+                return_string = "SLNK"
+            },
+            Self::Normal => {
+                return_string = "NRML"
+            },
+            Self::Sock => {
+                return_string = "SOCK"
+            },
+            Self::Whiteout => {
+                return_string = "WTOT"
+            },
+            Self::ArgDirectory => {
+                return_string = "ADIR"
+            },
+            Self::Fifo => {
+                return_string = "FIFO"
+            },
+            Self::Chardev => {
+                return_string = "CRDV"
+            },
+            Self::Blockdev => {
+                return_string = "BKDV"
+            },
+            Self::Unknown => {
+                return_string = "UKWN"
+            }
+        }
+
+        return return_string;
+    }
+
+    // helper functions
+
+    /**
+     * Determine if it is a socket
+     */
+    fn is_socket(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFSOCK
+    }
     
+    /**
+     * Determine if it is a symlink
+     */
+    fn is_symlink(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFLNK
+    }
+    
+    /**
+     * Determine if it is a regular file type
+     */
+    fn is_regular_file(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFREG
+    }
+    
+    /**
+     * Determine if it is a directory
+     */
+    fn is_directory(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFDIR
+    }
+    
+    /**
+     * Determine if it is a character device
+     */
+    fn is_character_device(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFCHR
+    }
+    
+    /**
+     * Determine if it is a block device
+     */
+    fn is_block_device(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFBLK
+    }
+    
+    /**
+     * Determine if it is a named pipe
+     */
+    fn is_fifo(mode: libc::mode_t) -> bool {
+        mode & libc::S_IFMT == S_IFIFO
+    }
+
+    // #TODO: implement whiteout and arg directory detection helper functions
 }
 
 /**
@@ -171,7 +304,9 @@ fn list_directory_minimal(path: &str) -> io::Result<()> {
 
     for content in contents {
         let content = content?;
-        print!("{}  ", content.path().display());
+        let content_to_match = content.path();
+        let content_type = FileType::get_type_short_name(&FileType::match_metadata(&content_to_match));
+        print!("[{} {}]  ", content_type, content.path().display());
     }
 
     Ok(())
